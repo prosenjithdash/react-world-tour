ea78ecc5100751e5915b78a7459ba278728c019d
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-07-20 18:29:17 +0600

    Grid appled done

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
-
-
+import './Countries.css'
 //1. Load data
 const Countries = () => {
     // State
@@ -16,7 +15,7 @@ const Countries = () => {
     return (
         <div>
             <h2>Countries:{countries.length}</h2>
-            <div className="">
+            <div className="country_container">
                 {
                     countries.map((country) => <Country key={country.cca3} country={country}
                     ></Country>)
