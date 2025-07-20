2b5435120a66187b27d24f60c0e11da96bfa7344
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-07-21 03:24:51 +0600

    Conditional CSS and conditional style

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -11,13 +11,36 @@ const Countries = () => {
         fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,flags,cca3,area')
             .then(res => res.json())
             .then(data =>setCountries(data))
-    },[])
+    }, [])
+    
+    // For Mark Visit State
+    const [VisitedCountries, setVisitedCountries] = useState([]);
+
+    // For Mark HandleEvent
+    const handleVisitedCountry = (country) => {
+        console.log(country);
+    }
+
     return (
         <div>
             <h2>Countries:{countries.length}</h2>
+            <hr />
+            <hr />
+            <div>
+                <h3>Visited Country</h3>
+                <ul>
+
+                </ul>
+            </div>
+            <hr />
+            <hr />
             <div className="country_container">
                 {
-                    countries.map((country) => <Country key={country.cca3} country={country}
+                    countries.map((country) => <Country
+                        key={country.cca3}
+                        country={country}
+                        setVisitedCountries={setVisitedCountries}
+                        handleVisitedCountry={handleVisitedCountry}
                     ></Country>)
                 }
             </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Country.css'
-const Country = ({ country }) => {
+const Country = ({ country, setVisitedCountries, handleVisitedCountry }) => {
     const { name, flags, region, cca3, area } = country;
     console.log(country)
 
@@ -14,7 +14,7 @@ const Country = ({ country }) => {
         setVisited(!visited);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited ?'visitedStyle':'non_VisitedStyle'}`}>
             <div>
                 <h3>Name: {name?.common}</h3>
                 <img src={flags?.png} alt="" />
@@ -29,6 +29,8 @@ const Country = ({ country }) => {
                     <button onClick={handleVisited}>{visited?'Completed':'Visit'}</button>
                     {/* {visited ? 'Visited Country.': 'I want to visit'} */}
                     
+                    {/* Mark visited Button */}
+                    <button onClick={handleVisitedCountry}>Mark Visited</button>
                 </div>
                
             </div>
